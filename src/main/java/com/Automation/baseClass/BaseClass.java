2e09b353422e2e2e1e033fc992b160c610f4a3c2
Sujata C <sujatac@Sujatas-MacBook-Pro-2.local>
2020-06-24 23:24:30 +0200

    Added Dockerfile and docker-compose.yml for Selenium Grid. Added configuration for Docker Image grid testing

--- src/main/java/com/Automation/baseClass/BaseClass.java
+++ src/main/java/com/Automation/baseClass/BaseClass.java
@@ -134,18 +134,17 @@ public class BaseClass {
 					case "chrome":
 						logger.info("Launching Chrome Browser in Docker Machine");
 						caps=DesiredCapabilities.chrome();
-						caps.setCapability(CapabilityType.BROWSER_NAME, "chrome");
 						break;
 					case "firefox":
 						logger.info("Launching Firefox Browser in Docker Machine");
 						caps=DesiredCapabilities.firefox();
-						caps.setCapability(CapabilityType.BROWSER_NAME, "firefox");
 						break;
 					default:
 						logger.error("Sorry No Browser found to launch for Option:"+browserName+" in Remote Instance");
 						Assert.fail("Sorry No Browser found to launch for Option:"+browserName+" in Remote Instance", new NoSuchSessionException());
 					}
 					setDriver(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),caps));
+					break;
 				default:
 					logger.error("Sorry No Platform found to launch for Option:"+browserName);
 					Assert.fail("Sorry No Platform found to launch for Option:"+browserName, new NoSuchSessionException());
